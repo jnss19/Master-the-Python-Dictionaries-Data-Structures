2db156a3e7c2e4c4a888a15a8606b0ecf5fb6f3f
thirdygayares <janusgumapas19@gmail.com>
2024-11-15 15:19:22 +0800

    add userdata.py

--- student_grades_dict.py
+++ student_grades_dict.py
@@ -1,1 +1,19 @@
-"print('Hello, World!')" 
+# Creating a dictionary of students and their grades
+students_grades = {
+    'Student1': 'B',
+    'Student2': 'C',
+    'Student3': 'A',
+    'Student4': 'B+',
+    'Student5': 'C-'
+}
+
+print("Students and their grades:", students_grades)
+
+
+print("Grade of third student:", students_grades['Student3'])
+
+students_grades['Student2'] = 'A'
+
+del students_grades['Student5']
+
+print("Last key-value pair in the dictionary:", list(students_grades.items())[-1])
